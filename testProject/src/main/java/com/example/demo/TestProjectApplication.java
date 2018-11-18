8619bf91a35e6a6cdfa2454340f589d840a36084
User <User@DESKTOP-SNGN0PS>
2018-11-18 23:20:57 +0900

    test 프로그램

---
+++ testProject/src/main/java/com/example/demo/TestProjectApplication.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class TestProjectApplication {
+
+	@RequestMapping("/")
+	String index() {
+		return "Hello World!";
+	}
+	
+	public static void main(String[] args) {
+		SpringApplication.run(TestProjectApplication.class, args);
+	}
+}
