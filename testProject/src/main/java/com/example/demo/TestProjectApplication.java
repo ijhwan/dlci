67a9b8b3ca82634c0bec117573b70df146b181a6
User <User@DESKTOP-SNGN0PS>
2018-11-21 02:54:10 +0900

    test

--- testProject/src/main/java/com/example/demo/TestProjectApplication.java
+++ testProject/src/main/java/com/example/demo/TestProjectApplication.java
@@ -11,7 +11,7 @@ public class TestProjectApplication {
 
 	@RequestMapping("/")
 	String index() {
-		return "Hello World! Hellow haha!!! testt";
+		return "Hello World! Hellow haha!!! testtt";
 	}
 	
 	public static void main(String[] args) {
